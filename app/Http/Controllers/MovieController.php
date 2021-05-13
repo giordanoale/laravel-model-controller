6a9c66592232b30ea2afef43e3ba53d3c3fec326
giordanoale <a.giordano89@gmail.com>
2021-05-13 19:04:58 +0200

    Creato model Movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movie= Movie::all();
+        return view('home');
+    }
 }
